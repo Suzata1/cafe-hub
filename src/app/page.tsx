ba26c8782ed51707adfb7d61f57bdaf386388eea
Suzata067 <pokhrelsujata1@gmail.com>
2025-06-12 20:04:08 +0545

    fix: build issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import NavBar from "@/components/navbar/NavBar";
+
 import HeroSection from "@/components/HeroSection";
 import ExploreSection from "@/components/ExploreSection";
 import TestimonialSection from "@/components/TestimonialSection";
--- src/components/ExploreSection.tsx
+++ src/components/ExploreSection.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
-import { text } from 'stream/consumers';
+
 
 const socialMedias = [
   {
